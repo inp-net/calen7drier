f0dc995a2e2ec12e26843b9ca1f55f6ff935e4c0
Ewen Le Bihan <hey@ewen.works>
2024-09-02 10:25:38 +0200

    🧑‍💻 Add restart deployment to push.fish

--- push.fish
+++ push.fish
@@ -6,3 +6,5 @@ docker build -t harbor.k8s.inpt.fr/net7/ade-feed-url:$v .
 docker build -t harbor.k8s.inpt.fr/net7/ade-feed-url .
 docker push harbor.k8s.inpt.fr/net7/ade-feed-url:$v
 docker push harbor.k8s.inpt.fr/net7/ade-feed-url
+sleep 10
+kubectl -n ade-feed-url rollout restart deployment ade-feed-url
